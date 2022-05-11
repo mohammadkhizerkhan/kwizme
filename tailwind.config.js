0d2b48bc8fee6eb7d493aed1b8691a3cf9e23b39
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-05-12 00:08:58 +0530

    bugs in tailwand

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"976px",
+      xl:"1440px"
+    },
+    extend: {
+      colors:{
+        primary:"#4ade80",
+        text__color:"#0E131F"
+        
+      }
+    },
   },
   plugins: [],
 }
