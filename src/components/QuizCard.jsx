ec248614abe42477a677fab58f2628e78c4ba5cb
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-05-13 00:46:56 +0530

    added images

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -4,51 +4,66 @@ function QuizCard() {
   return (
     <div className="flex flex-row flex-wrap justify-around gap-4 my-16 mx-24 sm:mx-5 md:mx-14">
       <div class="bg-white rounded-lg shadow-lg w-96">
-        <img
-          src="https://images.unsplash.com/photo-1600054800747-be294a6a0d26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"
-          alt=""
-          class="rounded-t-lg w-full"
-        />
+        <div className="h-64">
+          <img
+            src="https://wallpapercave.com/dwp1x/wp2581477.jpg"
+            alt=""
+            class="rounded-t-lg w-full h-full"
+          />
+        </div>
         <div class="py-4 px-3">
           <h2 class="font-bold mb-1 text-2xl text-purple-800">
-            Card with an image
+            Wanna Become Hokage
           </h2>
           <p class="text-purple-700 mb-4">
-           Take this quizz to test yourself <span className="text-xs grey">(5 question)</span>
+            Take this quizz to test yourself{" "}
+            <span className="text-xs grey">(5 question)</span>
           </p>
-          <button class="animate-bounce inline-block py-2 px-4 bg-primary text-2xl text_color rounded-lg">Play</button>
+          <button class="animate-bounce inline-block py-2 px-4 bg-primary text-2xl text_color rounded-lg">
+            Play
+          </button>
         </div>
       </div>
       <div class="bg-white rounded-lg shadow-lg w-96">
-        <img
-          src="https://images.unsplash.com/photo-1600054800747-be294a6a0d26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"
-          alt=""
-          class="rounded-t-lg w-full"
-        />
+        <div className="h-64">
+          <img
+            src="https://www.flixwatch.co/wp-content/uploads/70300472.jpg.webp"
+            alt=""
+            class="rounded-t-lg w-full h-full"
+          />
+        </div>
         <div class="py-4 px-3">
           <h2 class="font-bold mb-1 text-2xl text-purple-800">
-            Card with an image
+            Pass the Hunter Exam
           </h2>
           <p class="text-purple-700 mb-4">
-           Take this quizz to test yourself <span className="text-xs grey">(5 question)</span>
+            Take this quizz to test yourself{" "}
+            <span className="text-xs grey">(5 question)</span>
           </p>
-          <button class="animate-bounce inline-block py-2 px-4 bg-primary text-2xl text_color rounded-lg">Play</button>
+          <button class="animate-bounce inline-block py-2 px-4 bg-primary text-2xl text_color rounded-lg">
+            Play
+          </button>
         </div>
       </div>
       <div class="bg-white rounded-lg shadow-lg w-96">
-        <img
-          src="https://images.unsplash.com/photo-1600054800747-be294a6a0d26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"
-          alt=""
-          class="rounded-t-lg w-full"
-        />
+        <div className="h-64">
+          <img
+            src="https://wallpaperaccess.com/full/2953010.png"
+            alt=""
+            class="rounded-t-lg w-full h-full"
+          />
+        </div>
         <div class="py-4 px-3">
           <h2 class="font-bold mb-1 text-2xl text-purple-800">
-            Card with an image
+            Become Hashira
           </h2>
           <p class="text-purple-700 mb-4">
-           Take this quizz to test yourself <span className="text-xs grey">(5 question)</span>
+            Take this quizz to test yourself{" "}
+            <span className="text-xs grey">(5 question)</span>
           </p>
-          <button class="animate-bounce inline-block py-2 px-4 bg-primary text-2xl text_color rounded-lg">Play</button>
+          <button class="animate-bounce inline-block py-2 px-4 bg-primary text-2xl text_color rounded-lg">
+            Play
+          </button>
         </div>
       </div>
     </div>
