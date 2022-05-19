1a4d18a01953dc4fb0a54e11409d398078824dcd
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-05-19 15:10:44 +0530

    updated setDoc

--- src/services/AddUserScore.js
+++ src/services/AddUserScore.js
@@ -1,6 +1,4 @@
-import {
-  userDb
-} from "../firebase/config";
+import { userDb } from "../firebase/config";
 import {
   addDoc,
   doc,
@@ -9,15 +7,25 @@ import {
   where,
   collection,
   setDoc,
-  getDocs,
+  getDoc,
 } from "firebase/firestore";
 
-const AddUserScore =(score, user) => {
-    console.log(score,user)
-    setDoc(doc(userDb,user.uid),{
-        uid:user.uid,
-        score:score
-    },{ merge: true })
+const getUserScore=(user)=>{
+  return getDoc(doc(userDb, user.uid)).data();
+}
+
+const AddUserScore = (colName, score, user) => {
+  // console.log(getUserScore(user))
 };
 
 export { AddUserScore };
+
+// setDoc(
+  //   doc(userDb, user.uid),
+  //   {
+  //     uid: user.uid,
+  //     score: score,
+  //     category: colName,
+  //   },
+  //   { merge: true }
+  // );
